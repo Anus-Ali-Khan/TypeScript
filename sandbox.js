88c35eb4e73e4ba5a8154412966f9b3c1302a772
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-08-07 19:46:46 +0500

    Compilation

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-var character = 'mario';
+var character = 'khan';
 console.log(character);
 var inputs = document.querySelectorAll('input');
+console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
