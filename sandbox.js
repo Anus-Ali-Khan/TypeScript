128d7f6dafff07efd4884adf02a2d209a6932e31
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-08-07 21:34:53 +0500

    arrays

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,2 @@
-var character = 'khan';
-var age = 30;
-var isBlackBelt = false;
-// character = 20  {In ypeScript we cannot change the type of variable }
-character = 'anas'; //although we cn change its value
-// age = 'yoshi'
-age = 40;
-// isBlackBelt = 'yes'
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
+//arrays
+//objects
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,4 @@
-let character = 'khan';
-let age = 30;
-let isBlackBelt = false;
+//arrays
 
-// character = 20  {In ypeScript we cannot change the type of variable }
-character = 'anas' //although we cn change its value
 
-// age = 'yoshi'
-age = 40;
-
-// isBlackBelt = 'yes'
-isBlackBelt = true
-
-const circ = (diameter : number)=>{
-    return diameter * Math.PI;
-}
-
-console.log(circ(7.5))
+//objects
